a5bf1edec534893f994ca38f1cbded749d8f7124
fin <89432262+yuesong1@users.noreply.github.com>
2022-12-17 11:05:48 +0800

    initial commit

---
+++ artrepo/src/main/java/com/ethfin/artrepo/ArtrepoApplication.java
@@ -0,0 +1,13 @@
+package com.ethfin.artrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtrepoApplication.class, args);
+	}
+
+}
---
+++ artrepo/src/test/java/com/ethfin/artrepo/ArtrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethfin.artrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
