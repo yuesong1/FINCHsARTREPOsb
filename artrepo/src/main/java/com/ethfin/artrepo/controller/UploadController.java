9107f51f7262f47f3979b8e8a1fc12f7f69e9b97
fin <89432262+yuesong1@users.noreply.github.com>
2022-12-19 16:25:54 +0800

    add upload function

--- artrepo/src/main/java/com/ethfin/artrepo/controller/UploadController.java
+++ artrepo/src/main/java/com/ethfin/artrepo/controller/UploadController.java
@@ -2,19 +2,57 @@ package com.ethfin.artrepo.controller;
 
 import com.ethfin.artrepo.service.UploadService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/upload")
-@AllArgsConstructor
+//@RequestMapping("/upload")
+@RequiredArgsConstructor
 public class UploadController {
     private final UploadService uploadService;
-    @PostMapping
-    public void upload(){
-
+    @Value("${file.upload.path}")
+    private String fileUploadPath;
+    @GetMapping(value = "/upload")
+    public String uploadPage(){
+        return "upload page";
     }
+    @RequestMapping(value = "/upload",
+            method = RequestMethod.POST)
+    public int upload(@RequestParam("file") MultipartFile uploadFile){
+       try{
+        String path=fileUploadPath;
+        System.out.println("upload to: "+path);
+        File file=new File(path);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+          SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss_SS");
+        String filename=df.format(new Date().getTime());
+           //String filename=new Timestamp(System.currentTimeMillis()).toString();
+           String newName=filename+'.'+FilenameUtils.getExtension(uploadFile.
+                   getOriginalFilename());
+        uploadFile.transferTo(new File(file,newName));
+           System.out.println(newName);
+        return 200;
+
+    }catch(
+    IOException e){
+           System.out.println(e);
+           System.out.println("upload failed: "+ uploadFile.getOriginalFilename());
+            return 500;
+    }
+}
 }
--- artrepo/src/main/java/com/ethfin/artrepo/service/UploadService.java
+++ artrepo/src/main/java/com/ethfin/artrepo/service/UploadService.java
@@ -1,6 +1,12 @@
 package com.ethfin.artrepo.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class UploadService {
-        public void uploadFile()
+       // public void uploadFile();
+
+
 }
